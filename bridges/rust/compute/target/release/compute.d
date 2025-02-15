358a3451b0206726175d9afacfad5ddcf485e53d
enochthedev <enochjesse884@gmail.com>
2025-02-15 18:01:10 +0100

    remove unused Cargo.lock and build artifacts from the compute project

--- bridges/rust/compute/target/release/compute.d
+++
@@ -1,1 +0,0 @@
-/Users/user/Desktop/random\ code/project-J/bridges/rust/compute/target/release/compute: /Users/user/Desktop/random\ code/project-J/bridges/rust/compute/src/main.rs
--- bridges/rust/compute/target/release/deps/compute-97f45cea4ccce946.d
+++
@@ -1,5 +0,0 @@
-/Users/user/Desktop/random code/project-J/bridges/rust/compute/target/release/deps/compute-97f45cea4ccce946: src/main.rs
-
-/Users/user/Desktop/random code/project-J/bridges/rust/compute/target/release/deps/compute-97f45cea4ccce946.d: src/main.rs
-
-src/main.rs:
